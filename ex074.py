4ccdd64699767517c3f41e24be1a7c800ffef766
Diiegoorodrigo <diegorodrigosilva76@gmail.com>
2024-08-22 17:26:50 -0300

    test jet

--- ex074.py
+++ ex074.py
@@ -1,7 +1,7 @@
 # Crie um programa que insira 5 números aleatórios na tupla
 # mostre o maior número e o menor
 from random import randint
-n = (randint(1,100), randint(1,100), randint(1,100), randint(1,100), randint(1,100))
+n = (randint(1,200), randint(1,100), randint(1,100), randint(1,100), randint(1,100))
 nMn = nMr =0
 
 for c in n:
